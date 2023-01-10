a9a6314ff79d61d27a9fb7cb76acb4d52383fe92
Aviral Singh <aviral.singh.9794@gmail.com>
2023-01-10 21:29:39 +0530

    creating body of student class

---
+++ Student.java
@@ -0,0 +1,21 @@
+ /*This class maintains is responsible for keeping track of students
+     * name,
+     * class,
+     * grade,
+     * fees paid,
+     * fees left to be paid
+     */
+class Student{
+        private int id;
+        private String name;
+        private char grade;
+        private int class;
+        private int fees;
+        private int feeDue;
+        
+        public Student(int name, String name, int class){
+            this.id=id;
+            this.name=name;
+            this.class=class;
+        }
+}q
